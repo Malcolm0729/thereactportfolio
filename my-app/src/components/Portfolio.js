ac5ecfdf1133177e60f15509ff12b2652e91c6d5
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-28 22:46:22 -0700

    final commit

---
+++ my-app/src/components/Portfolio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import project1 from "../images/"
+import project2 from "../images/"
+import project3 from "../images/"
+import project4 from "../images/"
+import project5 from "../images/"
+import project6 from "../images/"
