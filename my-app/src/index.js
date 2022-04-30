fd48a26ce4a88001f66262855495701a2affa4e1
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-29 22:00:10 -0700

    final commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from 'react-router-dom'
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+/* GLOBAL VARIABLES */
 
-serviceWorker.unregister();
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
